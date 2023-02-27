b159aa227f226c706cce1dd604ceaa79441384cc
SinsOfSeven <JacobC.D.Wable@gmail.com>
2023-02-26 23:28:17 -0700

    changed output style

--- scripts/MemeMorial.ts
+++ scripts/MemeMorial.ts
@@ -9,22 +9,48 @@ import { createWriteStream } from "fs"
 import HardLevels from "../data/NPCLevelMetaData.json" assert {type:"json"}
 import ArenaBosses from "../data/SinDemonExMonsterMetaData.json" assert {type:"json"}
 import MonsterConfigs from "../data/MonsterConfigMetaData.json" assert {type:"json"}
-import UniqueMonstersa from "../data/UniqueMonsterMetaData.json" assert {type:"json"}
-
-var UniqueMonsters:UniqueMonster[] = UniqueMonstersa
+import UniqueMonsters from "../data/UniqueMonsterMetaData.json" assert {type:"json"}
 
 import { TextMap } from "../types/types";
 import LoadMap from "../generators/TextMapLoader.js"
 const text_map:TextMap = LoadMap("./generated/textmap_en.ndjson")
 
-
-//const PropertiesMap = new Map<number,MonsterConfigSimple>()
 const PropertiesMap = new Map<string,MonsterConfigSimple>()
 const BossMap = new Map<string,SinDemonEXSimple>()
 const HardMap = new Map<string,HardSimple>()
 const UniqueMap = new Map<string,UniqueMonsterSimple>()
+
 const writer = createWriteStream('./generated/BossMapNew.json')
 //const MyMap = new Map<number,MemorialBoss>()
+
+function createID(boss:SinDemonEXSimple):string{
+    switch(boss.BaseScore+boss.ExtraScore){
+        case 2400:
+        case 4800:
+        case 9600:
+        case 19200:
+        case 38400:
+        case 48000: 
+            return `${getBracket(boss.BossId)}_${getRank(boss.MonsterLevel)}_UP`
+
+        case 2000:
+        case 4000:
+        case 8000:
+        case 16000:
+        case 32000:
+        case 40000: 
+            return `${getBracket(boss.BossId)}_${getRank(boss.MonsterLevel)}_DW`
+
+        case 72000:
+        case 144000:
+        case 288000: 
+            return `${getBracket(boss.BossId)}_${getRank(boss.MonsterLevel)}_EX`
+            
+        default: 
+            return ""
+    }
+}
+
 function keyToString(key:KeyType):string{
     return key.Name+","
     +key.Type+","
@@ -57,7 +83,7 @@ function getAttribute(num:number):string{
         case 4: return "QUA"
         case 5: return "IMG"
         case 6: return "GXY"
-        default: return "Unknown"
+        default: return "UKNOWN"
     }
 }
 function getBracket(bossID:number):string{
@@ -172,20 +198,22 @@ setTimeout(()=>{
                     console.log("no matching hard level")
                 }else{
                     writer.write(JSON.stringify({
-                        //[key]:{
+                        [createID(boss)]:{
                             Name:unique?.Name,
-                            // MonsterID:unique.MonsterId,
-                            // BossID:boss.BossId,
-                            Bracket:getBracket(boss.BossId),
-                            Rank:getRank(boss.MonsterLevel),
-                            Attr:getAttribute(boss.Attribute),
-                            HP:boss.MonsterHp,
-                            ATK:Number((base.ATK*unique?.AtkRatio*hard.ATKRatio).toPrecision(6)),
-                            DEF:Number((base.DEF*unique?.DefRatio*hard.DEFRatio).toPrecision(6)),
-                            RES:hard?.EleRes,
-                            //Elite:unique.Elite,
-                            Score:boss.BaseScore+boss.ExtraScore,
-                        //}
+                            Stats:{
+                                Attr:getAttribute(boss.Attribute),
+                                HP:boss.MonsterHp,
+                                ATK:Number((base.ATK*unique?.AtkRatio*hard.ATKRatio).toPrecision(6)),
+                                DEF:Number((base.DEF*unique?.DefRatio*hard.DEFRatio).toPrecision(6)),
+                                RES:hard?.EleRes,
+                                Elite:base.Elite,
+                            },
+                            MetaData:{
+                                Rank:getRank(boss.MonsterLevel),
+                                Bracket:getBracket(boss.BossId),
+                                Score:boss.BaseScore+boss.ExtraScore,
+                            },
+                        }
                     })+",\n")
                 }   
             }
